a015726df6f7354c83eb1fafc885488556453a81
it21019722 <87459125+IT21019722@users.noreply.github.com>
2021-08-10 06:39:39 +0000

    lec 6 activity 1 and 2

---
+++ lec6Activity 1.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Item{
+  private:
+    int itemCode;
+    char name[20];
+    float price;
+
+  public:
+    void setItemDetails(int code, char itemName[]);
+    void setPrice(float p);
+    int getItemCode();
+    float getPrice();
+};
+void Item::setItemDetails(int code, char itemName[])
+{
+  itemCode = code;
+  strcpy(name, itemName);
+}
+void Item::setPrice(float p)
+{
+  price = p;
+}
+int Item::getItemCode()
+{
+  return itemCode;
+}
+float Item::getPrice()
+{
+  return price;
+}
+
+int main()
+{
+  Item item1;
+  int code;
+  char name[20];
+  float price;
+  cout<<"Enter the item code: ";
+  cin>>code;
+  cout<<"Enter the name of item: ";
+  cin>>name;
+  item1.setItemDetails(code, name);
+  cout<<"Enter the price of item: ";
+  cin>>price;
+  item1.setPrice(price);
+  
+  cout<<item1.getItemCode()<<endl;
+  cout<<item1.getPrice()<<endl;
+}
+
